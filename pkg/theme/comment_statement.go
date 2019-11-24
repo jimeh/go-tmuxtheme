54a2b04ff20c91f3a03cbeead222f5f613a36898
Jim Myhrberg <contact@jimeh.me>
2019-11-24 16:58:23 +0000

    feat(theme): Add CommentStatement struct

---
+++ pkg/theme/comment_statement.go
@@ -0,0 +1,57 @@
+package theme
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/kballard/go-shellquote"
+)
+
+var commandStatementCommands = []string{"#"}
+var commentStatementMatcher = regexp.MustCompile(`^\s*(#)(.*?)$`)
+
+type CommentStatement struct {
+	Msg string
+}
+
+func (s *CommentStatement) Parse(body string) error {
+	args, err := shellquote.Split(body)
+	if err != nil {
+		return err
+	} else if len(args) == 0 {
+		return &NotSupportedCommandError{"", commandStatementCommands}
+	}
+
+	match := commentStatementMatcher.FindStringSubmatch(args[0])
+	if len(match) < 3 {
+		return &NotSupportedCommandError{args[0], commandStatementCommands}
+	}
+
+	err = s.parseCommand(match[1])
+	if err != nil {
+		return err
+	}
+
+	msg := []string{}
+	if match[2] != "" {
+		msg = append(msg, match[2])
+	}
+	msg = append(msg, args[1:]...)
+
+	s.Msg = strings.Join(msg, " ")
+	return nil
+}
+
+func (s *CommentStatement) Execute() error {
+	return nil
+}
+
+func (s *CommentStatement) parseCommand(cmd string) error {
+	for _, supportedCommand := range commandStatementCommands {
+		if cmd == supportedCommand {
+			return nil
+		}
+	}
+
+	return &NotSupportedCommandError{cmd, commandStatementCommands}
+}
---
+++ pkg/theme/comment_statement_test.go
@@ -0,0 +1,64 @@
+package theme
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommentStatementInterfaceCompliance(t *testing.T) {
+	assert.Implements(t, (*Statement)(nil), &CommentStatement{})
+}
+
+var commentStatementParseTests = []struct {
+	body  string
+	msg   string
+	error error
+}{
+	{body: `# This is a comment`, msg: "This is a comment"},
+	{body: `#  This is a comment`, msg: "This is a comment"},
+	{body: `#  This is a comment `, msg: "This is a comment"},
+	{body: `#This is a comment`, msg: "This is a comment"},
+	{body: `#This is a comment `, msg: "This is a comment"},
+	{body: `#This is a comment  `, msg: "This is a comment"},
+	{body: `  # This is a comment`, msg: "This is a comment"},
+	{body: `  #  This is a comment`, msg: "This is a comment"},
+	{body: `  #  This is a comment `, msg: "This is a comment"},
+	{body: `  #This is a comment`, msg: "This is a comment"},
+	{body: `  #This is a comment `, msg: "This is a comment"},
+	{body: `  #This is a comment  `, msg: "This is a comment"},
+	{body: `#`, msg: ""},
+	{body: `   #`, msg: ""},
+	{body: `#    `, msg: ""},
+	{
+		body:  ``,
+		error: &NotSupportedCommandError{"", []string{"#"}},
+	},
+	{
+		body:  `set -g @foo "bar"`,
+		error: &NotSupportedCommandError{"set", []string{"#"}},
+	},
+	{
+		body:  `set -g @foo "bar" # This is a comment`,
+		error: &NotSupportedCommandError{"set", []string{"#"}},
+	},
+}
+
+func TestCommentStatementParse(t *testing.T) {
+	for _, tt := range commentStatementParseTests {
+		s := &CommentStatement{}
+
+		err := s.Parse(tt.body)
+
+		if tt.msg != "" {
+			assert.Equal(t, tt.msg, s.Msg)
+		}
+
+		if tt.error != nil {
+			assert.Error(t, err)
+			assert.Equal(t, tt.error, err)
+		} else {
+			assert.NoError(t, err)
+		}
+	}
+}
